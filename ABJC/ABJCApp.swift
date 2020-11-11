978a182852c0dbb521154f1d8bb9434d88e32c85
Noah Kamara <developer@noahkamara.com>
2020-11-11 13:58:18 +0100

    Initial Commit

---
+++ ABJC/ABJCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABJCApp.swift
+//  ABJC
+//
+//  Created by Noah Kamara on 11.11.20.
+//
+
+import SwiftUI
+
+@main
+struct ABJCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABJC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ABJC
+//
+//  Created by Noah Kamara on 11.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
