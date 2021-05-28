303ea3559582aeb86f8bb7f4a29767ff5459d51a
Stephen Byatt <47413006+stephenb10@users.noreply.github.com>
2021-05-28 16:44:49 +1000

    UI Changes: - Add jellyfin logo with text to assets - Create colour extension to use for background gradient - Redesign for server selection screen

--- ABJC/UI/AuthView/ServerSelectionView.swift
+++ ABJC/UI/AuthView/ServerSelectionView.swift
@@ -16,63 +16,107 @@ extension AuthView {
         /// Servers Discovered By ServerLookup
         @State var servers: [ServerLocator.ServerLookupResponse] = []
         
+        @State var searching = false
+        
         /// ServerLocator
         private let locator: ServerLocator = ServerLocator()
         
         var body: some View {
-            VStack
-            {
-                
-                ScrollView(.horizontal) {
+            
+            GeometryReader { geometry in
+                HStack {
+                    VStack {
+                        Image("logoWithText")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaleEffect(0.75)
+                    }
+                    .frame(width: geometry.size.width * 1/2)
                     
-                    // Automatic Server Selection
-                    LazyHStack(alignment: .center)
-                    {
-                        NavigationLink(destination: ManualView())
-                        {
-                            ServerCardView("authView.serverSelection.manual.label",
-                                           "authView.serverSelection.manual.descr")
-                        }
+                    VStack(alignment: .center) {
                         
-                        
-                        // Manual Server Selection
-                        ForEach(self.servers, id:\.id)
-                        { server in
-                            NavigationLink(
-                                destination: CredentialEntryView(server.host, Int(server.port), nil))
-                            {
-                                ServerCardView(server)
+                        VStack {
+                            HStack(spacing: 5) {
+                                Spacer()
+                                    .frame(width: 200)
+                                Text("authView.serverSelection.title")
+                                    .font(.title2).bold()
+                                ProgressView()
+                                    .frame(width: 200)
+                                    .hidden(!searching)
+                                
+                            }
+                            
+                            Group {
+                                ScrollView {
+                                    VStack(spacing: 10) {
+                                        
+                                        if self.servers.count > 0 {
+                                            ForEach(self.servers, id:\.id) { server in
+                                                NavigationLink(
+                                                    destination: CredentialEntryView(server.host, Int(server.port), nil))
+                                                {
+                                                    ServerCardView(server)
+                                                }
+                                                .padding()
+                                            }
+                                        }
+                                        
+                                        NavigationLink(destination: ManualView())
+                                        {
+                                            ServerCardView("authView.serverSelection.manual.label",
+                                                           "authView.serverSelection.manual.descr")
+                                        }
+                                        .padding()
+                                        
+                                    }
+                                }
+                                
                             }
                         }
+                        .frame(maxHeight: .infinity)
+                        
+                        
                     }
+                    .frame(width: geometry.size.width * 1/2)
                 }
-//                .onLongPressGesture {
-//                    DispatchQueue.main.async {
-//                        session.preferences.isDebugEnabled.toggle()
-//                        if session.preferences.isDebugEnabled {
-//                            session.alert = AlertError("pref.debugmode.title", "pref.debugmode.enabled")
-//                        } else {
-//                            session.alert = AlertError("pref.debugmode.title", "pref.debugmode.disabled")
-//                        }
-//                    }
-//                }
             }
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: Color.backgroundGradient),
+                    startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .edgesIgnoringSafeArea(.all))
             .onAppear(perform: discover)
-            .navigationTitle("authView.serverSelection.title")
+            //                .onLongPressGesture {
+            //                    DispatchQueue.main.async {
+            //                        session.preferences.isDebugEnabled.toggle()
+            //                        if session.preferences.isDebugEnabled {
+            //                            session.alert = AlertError("pref.debugmode.title", "pref.debugmode.enabled")
+            //                        } else {
+            //                            session.alert = AlertError("pref.debugmode.title", "pref.debugmode.disabled")
+            //                        }
+            //                    }
+            //                }
         }
         
         /// Discover Servers
         func discover() {
-            locator.locateServer { (server) in
-                if server != nil {
-                    if !servers.contains(server!) {
-                        servers.append(server!)
-                    }
+            searching = true
+            
+            // Timeout after 5 seconds
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                self.searching = false
+            }
+            
+            locator.locateServer { [self] (server) in
+                if let server = server, !servers.contains(server) {
+                    servers.append(server)
                 }
+                searching = false
             }
         }
     }
-
+    
     struct ServerSelectionView_Previews: PreviewProvider {
         static var previews: some View {
             ServerSelectionView()
