890a8c137fbbe7846d2ce512d4869d475a2ba1ab
Noah Kamara <mail@noahkamara.com>
2021-08-14 16:58:21 +0200

    removed analytics - to be considered at a later date

--- ABJC/ABJCApp.swift
+++ ABJC/ABJCApp.swift
@@ -7,23 +7,9 @@
 
 import SwiftUI
 import URLImage
-import NewRelic
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        NewRelic.disableFeatures([
-            .NRFeatureFlag_ExperimentalNetworkingInstrumentation,
-            .NRFeatureFlag_HttpResponseBodyCapture,
-            .NRFeatureFlag_NetworkRequestEvents
-        ])
-        NewRelic.start(withApplicationToken:"eu01xx3ffb20a4549b937ac5fe2e2c5b8f4f49c9b3-NRMA")
-        return true
-    }
-}
 
 @main
 struct ABJCApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     /// Session Store
     let session: SessionStore = SessionStore()
