81325103e6b18ec41baa3a0016395a957aa70e1a
Noah Kamara <mail@noahkamara.com>
2021-09-07 02:02:17 +0200

    completed analytics

---
+++ ABJC/PreferencesInfo.swift
@@ -0,0 +1,16 @@
+//
+//  PreferencesInfo.swift
+//  PreferencesInfo
+//
+//  Created by Noah Kamara on 05.09.21.
+//
+
+import Foundation
+
+public struct PreferencesInfo: AnalyticsData {
+    public static var type: String = AnalyticsDataType.preferences.rawVaue
+    
+    public var description: String
+    
+    
+}
