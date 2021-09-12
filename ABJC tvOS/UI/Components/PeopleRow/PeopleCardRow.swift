3c8cdd9aef11b47258fed5e441cda6a829ab7a89
Noah Kamara <mail@noahkamara.com>
2021-09-12 19:21:03 +0200

    readded PeopleCardRow

---
+++ ABJC tvOS/UI/Components/PeopleRow/PeopleCardRow.swift
@@ -0,0 +1,54 @@
+//
+//  PeopleCardRow.swift
+//  PeopleCardRow
+//
+//  Created by Noah Kamara on 10.09.21.
+//
+
+import SwiftUI
+import JellyfinAPI
+
+struct PeopleCardRow: View {
+    /// Edge Insets
+    private var edgeInsets = EdgeInsets(top: 30, leading: 85, bottom: 50, trailing: 0)
+    
+    /// Label
+    private let label: LocalizedStringKey
+    
+    /// Items
+    private let people: [BaseItemPerson]
+    
+    /// Initializer
+    /// - Parameters:
+    ///   - label: Localized Row Label
+    ///   - people: Row People
+    public init(_ label: LocalizedStringKey, _ people: [BaseItemPerson]) {
+        self.label = label
+        self.people = people
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .font(.title3)
+                .padding(.horizontal, edgeInsets.leading)
+            ScrollView(.horizontal) {
+                HStack(spacing: 48) {
+                    ForEach(people, id:\.id) { person in
+                        Button(action: {}) {
+                            PeopleCard(person)
+                        }
+                    }
+                }
+                .frame(height: 450)
+                .padding(edgeInsets)
+            }
+        }
+    }
+}
+
+struct PeopleCardRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PeopleCardRow("People Card Row", [])
+    }
+}
