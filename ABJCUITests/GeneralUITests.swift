19262f9c889b4eeb99553c0d64ec393e0e4767fd
Noah Kamara <mail@noahkamara.com>
2021-09-17 21:16:17 +0200

    Updated Gitignore

--- ABJCUITests/GeneralUITests.swift
+++ ABJCUITests/GeneralUITests.swift
@@ -13,10 +13,6 @@
 import XCTest
 
 class GeneralUITests: XCTestCase {
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
